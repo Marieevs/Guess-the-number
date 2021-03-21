13d5494f6a8852809f99a804e74a3be12a0c736a
Marieevs <m.yauseichyk@gmail.com>
2021-03-21 11:48:00 +0300

    Added changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,17 @@
 'use strict'
 import {areNumbersEqual, getRandomNumber} from "./utils.js";
+
 const result = getRandomNumber(1, 11);
-const inputNumber = prompt('Введите число от 1 до 10');
+const inputNumber = Number(prompt('Введите число от 1 до 10'));
+const comparison = areNumbersEqual(result, inputNumber);
 console.log(`Вы ввели ${inputNumber}`);
 console.log(`Сгенерированное число = ${result}`);
-alert(areNumbersEqual(result, inputNumber));
+
+if (comparison === true) {
+    alert('Вы угадали!!!');
+} else alert('Попробуйте сыграть еще раз');
+
+
 
 
 
--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -3,14 +3,16 @@ export function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 export function areNumbersEqual(result,inputNumber) {
-    if (Number(inputNumber) === result) {
-       return 'Вы угадали!!!';
-    } else if ((Number(inputNumber) > 10) || (Number(inputNumber) < 1) || (isNaN(inputNumber))) {
-        return 'Введите корректное значение и сыграйте ещё';
-    } else if (Number(inputNumber) !== result) {
-        return 'Вы не угадали, попробуйте сыграть ещё';
+    if (inputNumber === result) {
+       return true;
+    } else if ((inputNumber > 10) || (inputNumber < 1) || (isNaN(inputNumber))){
+        return false;
+    } else if (inputNumber !== result) {
+        return false;
     }
 }
+
+
 
 
 
